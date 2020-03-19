9bb04f2619700c506348280e2cb7f5a2836c0b0f
Damiano Giusti <damiano.giusti@molo17.com>
2020-03-19 18:08:45 +0100

    Basic implementation for displaying user data

--- app/src/main/java/com/molo17/couchbase/lite/MainActivity.kt
+++ app/src/main/java/com/molo17/couchbase/lite/MainActivity.kt
@@ -1,16 +1,61 @@
+/*
+ * Copyright (c) 2020 MOLO17
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.molo17.couchbase.lite
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.molo17.couchbase.lite.databinding.ActivityMainBinding
 
+/**
+ * Created by Damiano Giusti on 19/03/2020.
+ */
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel by viewModels<MainViewModel>()
     private lateinit var views: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         views = ActivityMainBinding.inflate(layoutInflater)
         setContentView(views.root)
+
+        viewModel.users().observe(this, Observer(this::onUsers))
+    }
+
+    private fun onUsers(users: List<User>) {
+        val displayableUsers = users.joinToString(
+            separator = "\n\n",
+            transform = this::userToString
+        )
+
+        views.usersTextView.text = displayableUsers
+    }
+
+    private fun userToString(user: User) = buildString {
+        append("Name: ")
+        append(user.name)
+        appendln()
+        append("Surname: ")
+        append(user.surname)
+        appendln()
+        append("Age: ")
+        append(user.age)
     }
 }
+
---
+++ app/src/main/java/com/molo17/couchbase/lite/MainViewModel.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020 MOLO17
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.molo17.couchbase.lite
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.couchbase.lite.Database
+import com.couchbase.lite.DatabaseConfiguration
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * Created by Damiano Giusti on 19/03/2020.
+ */
+class MainViewModel : ViewModel() {
+
+    private val database by lazy(LazyThreadSafetyMode.NONE) {
+        Database("database.db", DatabaseConfiguration())
+    }
+
+    private val usersLiveData by lazy(LazyThreadSafetyMode.NONE) {
+        select(User.KEY_NAME, User.KEY_SURNAME, User.KEY_AGE)
+            .from(database)
+            .where { User.KEY_TYPE equalTo User.TYPE }
+            .orderBy { User.KEY_NAME.ascending() }
+            .asObjectsFlow(::User)
+            .asLiveData(Dispatchers.IO)
+    }
+
+    fun users(): LiveData<List<User>> = usersLiveData
+
+}
+
+class User(map: Map<String, Any?>) {
+    val name: String by map
+    val surname: String by map
+    val age: Int by map
+
+    companion object {
+        const val KEY_NAME = "name"
+        const val KEY_SURNAME = "surname"
+        const val KEY_AGE = "surname"
+        const val KEY_TYPE = "type"
+        const val TYPE = "user"
+    }
+}
